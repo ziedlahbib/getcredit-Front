e0e9ac1b7259c0fc727f7b9e8281e71958c96fa0
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-04-07 01:02:41 +0100

    Update magasin-service.service.ts

--- src/app/service/magasin-service.service.ts
+++ src/app/service/magasin-service.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Magasin } from '../model/magasin';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MagasinServiceService {
 
-  constructor() { }
+  getmagasinssurl="/api/magasin/get-magasins";
+  getmagasinbyIdsurl="/api/magasin/get-magasin";
+  addmagsinUrl="/api/magasin/add-magasin";
+  modifiermagasinUrl="/api/magasin/update-magasin";
+  deletemagasinUrl="/api/magasin/delete-magasin";
+  constructor(private http : HttpClient) { }
+
+  getMagasins(): Observable<Magasin[]>{
+    return this.http.get<Magasin[]>(`${this.getmagasinssurl}`);
+
+  }
+  getMagasinById(ident :Number): Observable<Magasin>{
+    return this.http.get<Magasin>(`${this.getmagasinbyIdsurl}/${ident}`);
+
+  }
+  ajoutMagasin(magasin :Magasin): Observable<Magasin>{
+    return this.http.post<Magasin>(`${this.addmagsinUrl}`,magasin);
+  }
+  updateMagasin(id:Number, magasin:Magasin):Observable<Magasin>{
+    return this.http.put<Magasin>(`${this.modifiermagasinUrl}/${id}`,magasin);
+  }
+  deleteMagasin(id:number): any{
+    return this.http.delete(`${this.deletemagasinUrl}/${id}`);
+  }
 }
