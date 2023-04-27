8327d0b214ab651a4b275df151d7e046564d538a
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-04-27 16:18:57 +0100

    add produit html

--- src/app/pages/produit-management/add-produit/add-produit.component.ts
+++ src/app/pages/produit-management/add-produit/add-produit.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProduitServiceService } from 'src/app/service/produit-service.service';
 import jwt_decode from "jwt-decode";
 import { UserServiceService } from 'src/app/service/user-service.service';
+import { Magasin } from 'src/app/model/magasin';
+import { Entreprise } from 'src/app/model/entreprise';
+import { EntrepriseServiceService } from 'src/app/service/entreprise-service.service';
+import { MagasinServiceService } from 'src/app/service/magasin-service.service';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-add-produit',
@@ -11,11 +16,24 @@ import { UserServiceService } from 'src/app/service/user-service.service';
   styleUrls: ['./add-produit.component.scss']
 })
 export class AddProduitComponent {
+
+  
+  matFormFieldHidePlaceholder: boolean = false;
+  public magform: FormGroup;
+  public entform: FormGroup;
+  listofMagasin: Magasin[] = [];
+  listofEntreprise: Entreprise[]
+  public role: string | null;
   public produiteform: FormGroup;
   constructor(private ps: ProduitServiceService, private formBuilder: FormBuilder, private route: Router,
-    private us: UserServiceService) { }
+    private us: UserServiceService, 
+    private es: EntrepriseServiceService, private ms: MagasinServiceService, private cdr: ChangeDetectorRef) { }
   ngOnInit(): void {
     this.initForm();
+    this.magasinform();
+    this.entrepriseform();
+    this.getrole();
+    this.getentreprise();
   }
   initForm() {
     this.produiteform = this.formBuilder.group({
@@ -28,6 +46,32 @@ export class AddProduitComponent {
     this.produiteform.valueChanges.subscribe(
       data => {
         console.log(this.produiteform.value);
+
+      }
+    )
+  }
+  magasinform() {
+    this.magform = this.formBuilder.group({
+      magasinId: ['', Validators.required],
+    });
+
+
+    this.magform.valueChanges.subscribe(
+      data => {
+        console.log(this.magform.value);
+
+      }
+    )
+  }
+  entrepriseform() {
+    this.entform = this.formBuilder.group({
+      entrpriseId: ['', Validators.required],
+    });
+
+
+    this.entform.valueChanges.subscribe(
+      data => {
+        console.log(this.entform.value);
 
       }
     )
@@ -49,6 +93,42 @@ export class AddProduitComponent {
 
           }
         )
+      }
+    )
+  }
+  ///////////////////////////////////////:
+  getrole() {
+    this.role = localStorage.getItem('role' || '');
+    console.log(this.role)
+  }
+  getentreprise() {
+    let token = localStorage.getItem('autorisation' || '');
+    let user: any = jwt_decode(token || '');
+    this.us.getuserById(user.jti).subscribe(
+      data => {
+
+        this.es.getEntrepriseByentrepreneur(data.id).subscribe(
+          res => {
+            this.listofEntreprise = res;
+
+          }
+        )
+      }
+    )
+  }
+  getmagasins(event: MatSelectChange) {
+    const value = event.value;
+    //const value = this.entform.get(['entrpriseId'])?.value
+    console.log(value)
+    this.ms.getmagasinsbyentreprise(Number(value)).subscribe(
+      res => {
+        console.log(res)
+        this.listofMagasin = res;
+        // Make changes to the component's data
+        this.matFormFieldHidePlaceholder = false;
+
+        // Manually trigger a change detection cycle
+        this.cdr.detectChanges();
       }
     )
   }
